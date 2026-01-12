092cdeb88787b89b36372cb908d50042a1640b3f
Ostap Matsko <30385865+Ostaps@users.noreply.github.com>
2026-01-12 17:55:20 +0200

    Include registration router in user routes

--- api/routers/v1.py
+++ api/routers/v1.py
@@ -7,8 +7,10 @@ from fastapi import APIRouter
 from api.articles.routers import router as article_router
 from api.auth.routers import router as auth_router
 from api.users.routers import router as user_router
+from api.users.routers import registration_router
 
 router = APIRouter()
+router.include_router(registration_router, prefix="/users")
 router.include_router(user_router, prefix="/api/users")
 router.include_router(auth_router, prefix="/api/auth")
 router.include_router(article_router, prefix="/api/articles")
